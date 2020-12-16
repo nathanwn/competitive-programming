4f6a0dc8fc7d0eedd50cadb98dab3dd6d3aadf43
nathan-wien <nhat.nguyen.cs17@gmail.com>
2020-12-16 10:35:10 +1000

    Got K to to be fast enough

--- archive/icpc/south-pacific-regional/2015/k/main.cc
+++ archive/icpc/south-pacific-regional/2015/k/main.cc
@@ -2,17 +2,14 @@
 
 using namespace std;
 
-char buf[10];
-
-int base62ToDec(char c) {
-    if ('0' <= c && c <= '9') {
-        return c - '0';
-    }
-    if ('A' <= c && c <= 'Z') { 
-        return c - 'A' + 10;
-    }
-    assert('a' <= c && c <= 'z');
+int toDec(char c) {
+    if ('0' <= c && c <= '9') return c - '0';
+    if ('A' <= c && c <= 'Z') return c - 'A' + 10;
     return c - 'a' + 10 + 26;
+}
+
+int toDec(char* s) {
+    return toDec(s[0]) * 62 * 62 + toDec(s[1]) * 62 + toDec(s[2]);
 }
 
 struct Stadium {
@@ -21,8 +18,7 @@ struct Stadium {
     int to;
 
     Stadium(int city_, int from_, int to_) :
-        city(city_), from(from_), to(to_) {
-    }
+        city(city_), from(from_), to(to_) {}
 };
 
 struct Solver {
@@ -30,10 +26,12 @@ struct Solver {
     vector<Stadium> stadiums;
 
     Solver(int _n) : n(_n) {
+        char s[10];
+
         for (int i = 0; i < n; i++) {
-            assert(scanf("%s", buf) != EOF);
-            int from = base62ToDec(buf[0]) * 3844 + base62ToDec(buf[1]) * 62 + base62ToDec(buf[2]);
-            int to = base62ToDec(buf[3]) * 3844 + base62ToDec(buf[4]) * 62 + base62ToDec(buf[5]);
+            cin >> s;
+            int from = toDec(s);
+            int to = toDec(s + 3);
             stadiums.emplace_back(i, from, to);
         }
     }
@@ -43,10 +41,15 @@ struct Solver {
             return true;
         }
 
-        for (int i = from; i <= to; i++) {
+        for (int i = from, j = to; i <= j; i++, j--) {
             if (stadiums[i].from <= from && to <= stadiums[i].to) {
                 return isGreat(from, stadiums[i].city - 1)
                     && isGreat(stadiums[i].city + 1, to);
+            }
+
+            if (stadiums[j].from <= from && to <= stadiums[j].to) {
+                return isGreat(from, stadiums[j].city - 1)
+                    && isGreat(stadiums[j].city + 1, to);
             }
         }
 
@@ -77,7 +80,8 @@ struct Solver {
 
 int main() {
     int n;
-    while (scanf("%d", &n) != EOF) {
+
+    while (cin >> n) {
         Solver solver(n);
         solver.solve();
     }
