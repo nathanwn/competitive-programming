0613615cb412bc156fb23cea3beb9c593e8d3d82
nathan-wien <nhat.nguyen.cs17@gmail.com>
2021-01-15 22:25:46 +1000

    Add 2018 SPR F

--- archive/icpc/south-pacific-regional/2018/f/slow.cc
+++ archive/icpc/south-pacific-regional/2018/f/slow.cc
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+/**
+ * We can use matrix to calculate the shortest path of length k between any pair
+ * of vertices in the graph. For more details, refer to page 222 of the CSES book.
+ *
+ * Time complexity of this solution: O(n^3 * m) where n <= 100 and m <= 10^9
+ * which is a bit too slow.
+ *
+ * Can be further optimized using binary search + a technique which is similar to
+ * binary exponentiation.
+**/
+
 const int64_t inf = 1e18;
 
 template <typename T>
@@ -15,37 +26,39 @@ int main() {
     int n, m;
     cin >> n >> m;
     vec2d<int64_t> g(n, n, inf);
-    vec2d<int64_t> g0(n, n, inf);
-    vec2d<int64_t> gp(n, n, inf);
+    vec2d<int64_t> g1(n, n, inf);
+    vec2d<int64_t> prev_g(n, n, inf);
 
     for (int u = 0; u < n; u++) {
         for (int v = 0; v < n; v++) {
             int w;
             cin >> w;
-            g[u][v] = g0[u][v] = w;
+            g[u][v] = g1[u][v] = w;
         }
     }
 
     int path_length = 0;
 
     while (true) {
+        int64_t min_cost = inf;
         int stop = true;
+
         for (int v = 0; v < n; v++) {
-            if (g[0][v] <= m) stop = false;
+            min_cost = min(min_cost, g[0][v]);
         }
 
-        if (stop) break;
+        if (min_cost > m) break;
 
         path_length++;
 
-        swap(g, gp);
+        swap(g, prev_g);
 
         for (int u = 0; u < n; u++) {
             for (int v = 0; v < n; v++) {
                 g[u][v] = inf;
 
                 for (int k = 0; k < n; k++) {
-                    g[u][v] = min(g[u][v], gp[u][k] + g0[k][v]);
+                    g[u][v] = min(g[u][v], prev_g[u][k] + g1[k][v]);
                 }
             }
         }
