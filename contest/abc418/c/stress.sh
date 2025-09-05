818e4056519e8227a682d231c3c5a095961b670d
nathanwn <49882031+nathanwn@users.noreply.github.com>
2025-09-06 02:55:42 +1000

    move more files around

--- contest/abc418/c/stress.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for((i = 1; ; ++i)); do
-    echo $i
-    python3 gen.py $i > inp.txt
-    ./main < inp.txt > out.txt
-    ./slow < inp.txt > ans.txt
-    diff -w out.txt ans.txt || break
-done
